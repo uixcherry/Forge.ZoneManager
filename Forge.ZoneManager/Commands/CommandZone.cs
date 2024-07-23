269568f74983e40ce125f32027e3a729d5754649
uixcherry <uixcherry@gmail.com>
2024-07-24 01:51:05 +0500

    #

--- Forge.ZoneManager/Commands/CommandZone.cs
+++ Forge.ZoneManager/Commands/CommandZone.cs
@@ -2,6 +2,7 @@
 using Rocket.Unturned.Chat;
 using Rocket.Unturned.Player;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Forge.ZoneManager.Commands
 {
@@ -11,7 +12,7 @@ namespace Forge.ZoneManager.Commands
         public string Name => "zone";
         public string Help => "Manage zones on the server.";
         public string Syntax => "/zone <command> [arguments]";
-        public List<string> Aliases => new List<string>();
+        public List<string> Aliases => new List<string> { "z" };
         public List<string> Permissions => new List<string> { "forge.zone" };
 
         public void Execute(IRocketPlayer caller, string[] command)
@@ -26,60 +27,41 @@ namespace Forge.ZoneManager.Commands
 
             switch (command[0].ToLower())
             {
-                case "help":
-                    HandleHelpCommand(player);
+                case "add":
+                    HandleAddCommand(player, command);
                     break;
 
-                case "add":
-                    if (command.Length < 2)
-                    {
-                        UnturnedChat.Say(player, "Usage: /zone add <zone|node|flag|block|group|message> [arguments]");
-                        return;
-                    }
-                    switch (command[1].ToLower())
-                    {
-                        case "zone":
-                            HandleAddZoneCommand(player, command);
-                            break;
-                        case "node":
-                            HandleAddNodeCommand(player, command);
-                            break;
-                        case "flag":
-                            HandleAddFlagCommand(player, command);
-                            break;
-                        case "block":
-                            HandleAddBlockCommand(player, command);
-                            break;
-                        case "group":
-                            HandleAddGroupCommand(player, command);
-                            break;
-                        case "message":
-                            HandleAddMessageCommand(player, command);
-                            break;
-                        default:
-                            UnturnedChat.Say(player, "Invalid subcommand for /zone add");
-                            break;
-                    }
+                case "remove":
+                    HandleRemoveCommand(player, command);
+                    break;
+
+                case "replace":
+                    HandleReplaceCommand(player, command);
+                    break;
+
+                case "list":
+                    HandleListCommand(player, command);
+                    break;
+
+                case "inzone":
+                    HandleInZoneCommand(player, command);
+                    break;
+
+                case "tp":
+                    HandleTpCommand(player, command);
+                    break;
+
+                case "flags":
+                    HandleFlagsCommand(player, command);
+                    break;
+
+                case "blocklist":
+                    HandleBlockListCommand(player, command);
                     break;
 
                 case "visualize":
-                    if (command.Length < 2)
-                    {
-                        UnturnedChat.Say(player, "Usage: /zone visualize <nodes|border> <zonename> <on|off>");
-                        return;
-                    }
-                    switch (command[1].ToLower())
-                    {
-                        case "nodes":
-                            HandleVisualizeNodesCommand(player, command);
-                            break;
-                        case "border":
-                            HandleVisualizeBorderCommand(player, command);
-                            break;
-                        default:
-                            UnturnedChat.Say(player, "Invalid subcommand for /zone visualize");
-                            break;
-                    }
+                case "show":
+                    HandleVisualizeOrShowCommand(player, command);
                     break;
 
                 default:
@@ -90,56 +72,238 @@ namespace Forge.ZoneManager.Commands
 
         private void HandleHelpCommand(UnturnedPlayer player)
         {
-            
+            UnturnedChat.Say(player, Syntax);
         }
 
         private void HandleAddCommand(UnturnedPlayer player, string[] command)
         {
-            
+            if (command.Length < 2)
+            {
+                UnturnedChat.Say(player, "Usage: /zone add <zone|node|flag|block|group|parameter|heightnode> [arguments]");
+                return;
+            }
+            switch (command[1].ToLower())
+            {
+                case "zone":
+                    HandleAddZoneCommand(player, command);
+                    break;
+                case "node":
+                    HandleAddNodeCommand(player, command);
+                    break;
+                case "flag":
+                    HandleAddFlagCommand(player, command);
+                    break;
+                case "block":
+                    HandleAddBlockCommand(player, command);
+                    break;
+                case "group":
+                    HandleAddGroupCommand(player, command);
+                    break;
+                case "parameter":
+                    HandleAddParameterCommand(player, command);
+                    break;
+                case "heightnode":
+                    HandleAddHeightNodeCommand(player, command);
+                    break;
+                default:
+                    UnturnedChat.Say(player, "Invalid subcommand for /zone add");
+                    break;
+            }
         }
 
-        private void HandleVisualizeCommand(UnturnedPlayer player, string[] command)
+        private void HandleRemoveCommand(UnturnedPlayer player, string[] command)
+        {
+            if (command.Length < 2)
+            {
+                UnturnedChat.Say(player, "Usage: /zone remove <zone|node|flag|block|group|parameter|heightnode> [arguments]");
+                return;
+            }
+            switch (command[1].ToLower())
+            {
+                case "zone":
+                    HandleRemoveZoneCommand(player, command);
+                    break;
+                case "node":
+                    HandleRemoveNodeCommand(player, command);
+                    break;
+                case "flag":
+                    HandleRemoveFlagCommand(player, command);
+                    break;
+                case "block":
+                    HandleRemoveBlockCommand(player, command);
+                    break;
+                case "group":
+                    HandleRemoveGroupCommand(player, command);
+                    break;
+                case "parameter":
+                    HandleRemoveParameterCommand(player, command);
+                    break;
+                case "heightnode":
+                    HandleRemoveHeightNodeCommand(player, command);
+                    break;
+                default:
+                    UnturnedChat.Say(player, "Invalid subcommand for /zone remove");
+                    break;
+            }
+        }
+
+        private void HandleReplaceCommand(UnturnedPlayer player, string[] command)
         {
             
         }
+
+        private void HandleListCommand(UnturnedPlayer player, string[] command)
+        {
+            
+        }
+
+        private void HandleInZoneCommand(UnturnedPlayer player, string[] command)
+        {
+            
+        }
+
+        private void HandleTpCommand(UnturnedPlayer player, string[] command)
+        {
+            
+        }
+
+        private void HandleFlagsCommand(UnturnedPlayer player, string[] command)
+        {
+            
+        }
+
+        private void HandleBlockListCommand(UnturnedPlayer player, string[] command)
+        {
+            if (command.Length < 2)
+            {
+                UnturnedChat.Say(player, "Usage: /zone blocklist <add|remove|list|additem|removeitem> [arguments]");
+                return;
+            }
+            switch (command[1].ToLower())
+            {
+                case "add":
+                    HandleBlockListAddCommand(player, command);
+                    break;
+                case "remove":
+                    HandleBlockListRemoveCommand(player, command);
+                    break;
+                case "list":
+                    HandleBlockListListCommand(player, command);
+                    break;
+                case "additem":
+                    HandleBlockListAddItemCommand(player, command);
+                    break;
+                case "removeitem":
+                    HandleBlockListRemoveItemCommand(player, command);
+                    break;
+                default:
+                    UnturnedChat.Say(player, "Invalid subcommand for /zone blocklist");
+                    break;
+            }
+        }
+
+        private void HandleVisualizeOrShowCommand(UnturnedPlayer player, string[] command)
+        {
+            if (command.Length < 4)
+            {
+                UnturnedChat.Say(player, "Invalid! Try /zone visualize <nodes|border> <zonename> <on|off> <space>", Color.red);
+                return;
+            }
+        }
+
         private void HandleAddZoneCommand(UnturnedPlayer player, string[] command)
         {
-
+            
         }
 
         private void HandleAddNodeCommand(UnturnedPlayer player, string[] command)
         {
-
+            
         }
 
         private void HandleAddFlagCommand(UnturnedPlayer player, string[] command)
         {
-
+            
         }
 
         private void HandleAddBlockCommand(UnturnedPlayer player, string[] command)
         {
-
+            
         }
 
         private void HandleAddGroupCommand(UnturnedPlayer player, string[] command)
         {
-
+            
         }
 
-        private void HandleAddMessageCommand(UnturnedPlayer player, string[] command)
+        private void HandleAddParameterCommand(UnturnedPlayer player, string[] command)
         {
-
+            
         }
 
-        private void HandleVisualizeNodesCommand(UnturnedPlayer player, string[] command)
+        private void HandleAddHeightNodeCommand(UnturnedPlayer player, string[] command)
         {
-
+            
         }
 
-        private void HandleVisualizeBorderCommand(UnturnedPlayer player, string[] command)
+        private void HandleRemoveZoneCommand(UnturnedPlayer player, string[] command)
         {
+            
+        }
 
+        private void HandleRemoveNodeCommand(UnturnedPlayer player, string[] command)
+        {
+            
+        }
+
+        private void HandleRemoveFlagCommand(UnturnedPlayer player, string[] command)
+        {
+            
+        }
+
+        private void HandleRemoveBlockCommand(UnturnedPlayer player, string[] command)
+        {
+            
+        }
+
+        private void HandleRemoveGroupCommand(UnturnedPlayer player, string[] command)
+        {
+            
+        }
+
+        private void HandleRemoveParameterCommand(UnturnedPlayer player, string[] command)
+        {
+            
+        }
+
+        private void HandleRemoveHeightNodeCommand(UnturnedPlayer player, string[] command)
+        {
+            
+        }
+
+        private void HandleBlockListAddCommand(UnturnedPlayer player, string[] command)
+        {
+            
+        }
+
+        private void HandleBlockListRemoveCommand(UnturnedPlayer player, string[] command)
+        {
+            
+        }
+
+        private void HandleBlockListListCommand(UnturnedPlayer player, string[] command)
+        {
+            
+        }
+
+        private void HandleBlockListAddItemCommand(UnturnedPlayer player, string[] command)
+        {
+            
+        }
+
+        private void HandleBlockListRemoveItemCommand(UnturnedPlayer player, string[] command)
+        {
+            
         }
     }
 }
